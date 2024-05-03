2085b000961c3f866c4e00a3d34693b13979cfa6
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-05-02 20:54:02 -0500

    TodoList

---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+function EmptyTodos() {
+  return (
+    <div className='container-text'>
+      <p>¡Crea tu primer TO DO!</p>
+    </div>
+  );
+}
+
+export { EmptyTodos };
